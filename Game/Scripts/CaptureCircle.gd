5a9faf05cdbec5bd7598c6f49fcfa1dfe8c148d1
SaitenHazard <khantowsifkhan@gmail.com>
2021-10-10 23:15:48 +0600

    10/10/2021 11:15PM

--- Game/Scripts/CaptureCircle.gd
+++ Game/Scripts/CaptureCircle.gd
@@ -8,11 +8,11 @@ var line_lenght : int = 0
 
 onready var line = $Line2D
 onready var afterLine = $AfterLine2D
-var max_line_length : int = 200
+var max_line_length : int = 1000
 var lenght_between_points : int = 5
 
 var shape_begin_index : int
-var pointC : Vector2
+var segment_segment_intersection : Vector2
 
 var shape_set : bool = false
 var critter_touched : bool = false
@@ -29,12 +29,7 @@ func get_max_line_length():
 func get_line_length():
 	return line_lenght
 
-#func _physics_process(delta):
-#	_find_segemnt_critter_intersection()
-
 func _process(delta):
-#	_find_segemnt_critter_intersection()
-	
 	if TOUCH_STATE.RELEASED:
 		critter_touched = false
 		
@@ -96,12 +91,11 @@ func _draw_line():
 
 func _draw_shape():
 	points_shape = PoolVector2Array()
-	points_shape.append(pointC)
+	points_shape.append(segment_segment_intersection)
 	
 	for x in range(shape_begin_index, points_line.size()-2):
 		points_shape.append(points_line[x])
-		
-	points_shape.append(pointC)
+	points_shape.append(segment_segment_intersection)
 	shape_set = true
 	
 	line.points = PoolVector2Array()
@@ -120,11 +114,12 @@ func _find_segment_segment_intersection():
 	for x in (points_line.size() - 4):
 		var segment_from = points_line[x]
 		var segment_to = points_line[x+1]
-		var segment_segment_intersection = Geometry.segment_intersects_segment_2d(
+		var intersection = Geometry.segment_intersects_segment_2d(
 			latest_segment_from, latest_segment_to, segment_from, segment_to)
 			
-		if not segment_segment_intersection == null:
-			pointC = segment_segment_intersection
+		if not intersection == null:
+			shape_begin_index = x+1
+			segment_segment_intersection = intersection
 			_draw_shape()
 			_find_critter_inside_shape()
 			_initialize_draw_line()
